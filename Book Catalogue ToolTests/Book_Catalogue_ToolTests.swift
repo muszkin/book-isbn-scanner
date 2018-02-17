e3f664179643ed13239aa162a0a408e78bc1d896
muszkin <piotr.mucha@shoper.pl>
2018-02-17 16:25:08 +0100

    Initial Commit

---
+++ Book Catalogue ToolTests/Book_Catalogue_ToolTests.swift
@@ -0,0 +1,36 @@
+//
+//  Book_Catalogue_ToolTests.swift
+//  Book Catalogue ToolTests
+//
+//  Created by Piotr Mucha on 17.02.2018.
+//  Copyright © 2018 Piotr Mucha. All rights reserved.
+//
+
+import XCTest
+@testable import Book_Catalogue_Tool
+
+class Book_Catalogue_ToolTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
